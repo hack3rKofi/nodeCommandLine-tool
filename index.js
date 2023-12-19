8d48d4cbaa04d9585ef45667486422058f96b607
Boateng Gideon <hack3rkofi@gmail.com>
2023-12-19 21:21:55 +0000

    complete cmdline project with node

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 #!/usr/bin/env node
 const fs = require('fs');
+const chalk = require('chalk');
+const path = require('path');
 
 //implementation using promises
 const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+//add additional parameter from process.argv to get files in any dir
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
   if (err) {
     console.log('Error ', err);
   }
 
-  for (let filename of filenames) {
-    try {
-      const stats = await lstat(filename);
-      console.log(filename, stats.isFile());
-    } catch (error) {
-      console.log(error);
+  const statsPromise = filenames.map((filename) =>
+    lstat(path.join(targetDir, filename))
+  );
+  const allstats = await Promise.all(statsPromise);
+
+  for (const stats of allstats) {
+    const index = allstats.indexOf(stats);
+
+    if (stats.isFile()) {
+      console.log(filenames[index]);
+    } else {
+      console.log(chalk.redBright.bold(filenames[index]));
     }
   }
 });
